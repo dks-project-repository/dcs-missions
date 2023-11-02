b357c6790b00c2f10245ed709f484a5aa50cd79b
kyedlin <kyedlin@hotmail.com>
2023-11-02 02:33:06 -0700

    Missions

---
+++ Missions/Kevin.diff.lua
@@ -0,0 +1,28 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2004cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "View Center",
+		},
+	},
+}
+return diff
